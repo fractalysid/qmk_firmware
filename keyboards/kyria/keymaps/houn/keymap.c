3614c74ee72bf17e0798993f4ab248d4b60a6afb
houn <houn@digitalarcanum.net>
2021-03-23 09:40:36 -0700

    Removing home row mods

--- keyboards/kyria/keymaps/houn/keymap.c
+++ keyboards/kyria/keymaps/houn/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [QWERTY] = LAYOUT(
       KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLASH,
-      KC_TAB, MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D), MT(MOD_LCTL, KC_F),   KC_G,   KC_H,    MT(MOD_RCTL, KC_J), MT(MOD_RSFT, KC_K), MT(MOD_LALT, KC_L), MT(MOD_RGUI, KC_SCLN), KC_QUOT,
+      KC_TAB,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
       KC_LCTL, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   XXXXXXX,   KC_DEL, KC_BSPC, KC_LEAD,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
               KC_LGUI, KC_LALT, MO(LOWER), KC_LSFT,  KC_ENT, KC_RSFT, LT(NAV, KC_SPC),  MO(RAISE), KC_TAB, KC_RALT
     ),
